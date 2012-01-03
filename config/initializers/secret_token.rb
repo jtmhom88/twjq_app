d5d67fb9da29a4044bc0db02e1cc3594d02f2ee2
Johnny Hom <johnnyhom@gmail.com>
2012-01-03 10:25:08 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwjqApp::Application.config.secret_token = '4b6e43c60b1e7127608bdf8497a07bffc0538324c370e664ece96e0b2f9f099b24e70bf78e067a44389741c36a7f352fbd872086987b886a67b93fbf84781152'
